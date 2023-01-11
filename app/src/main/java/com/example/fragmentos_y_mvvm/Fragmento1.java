9f1e88eb9c795def52e2dae08098217f49b4852f
Nil Pedrós Cabrillana <nilpedros@ceroca.cat>
2023-01-11 20:26:34 +0100

    Cargando fragmentos 1 y 2

---
+++ app/src/main/java/com/example/fragmentos_y_mvvm/Fragmento1.java
@@ -0,0 +1,23 @@
+package com.example.fragmentos_y_mvvm;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Fragmento1 extends Fragment {
+
+    public Fragmento1() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragmento_1, container, false);
+    }
+}
--- app/src/main/java/com/example/fragmentos_y_mvvm/MainActivity.java
+++ app/src/main/java/com/example/fragmentos_y_mvvm/MainActivity.java
@@ -2,13 +2,42 @@ package com.example.fragmentos_y_mvvm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.UiAutomation;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mBtnCargarFragmento1, mBtnCargarFragmento2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mBtnCargarFragmento1 = findViewById(R.id.BTN_Fragmento1);
+        mBtnCargarFragmento2 = findViewById(R.id.BTN_Fragmento2);
+
+        mBtnCargarFragmento1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Cargamos el fragmento1
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.FL_ContenidoFragmentos, new Fragmento1())
+                        .commit();
+            }
+        });
+
+        mBtnCargarFragmento2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Cargamos el fragmento2
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.FL_ContenidoFragmentos, new Fragmento2())
+                        .commit();
+            }
+        });
     }
 }
