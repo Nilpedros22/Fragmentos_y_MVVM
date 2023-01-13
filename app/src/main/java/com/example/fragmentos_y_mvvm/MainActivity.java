9aaf215dfd9b22a475e715529a4ba42b50335a8e
Nil Pedrós Cabrillana <nilpedros@ceroca.cat>
2023-01-13 19:05:29 +0100

    Formularios fragmetos creados

--- app/src/main/java/com/example/fragmentos_y_mvvm/MainActivity.java
+++ app/src/main/java/com/example/fragmentos_y_mvvm/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.fragmentos_y_mvvm;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.app.UiAutomation;
 import android.content.DialogInterface;
@@ -20,13 +21,19 @@ public class MainActivity extends AppCompatActivity {
         mBtnCargarFragmento1 = findViewById(R.id.BTN_Fragmento1);
         mBtnCargarFragmento2 = findViewById(R.id.BTN_Fragmento2);
 
+        CargarFragmento(new Fragmento1());
+        /*getSupportFragmentManager().beginTransaction()
+                .replace(R.id.FL_ContenidoFragmentos, new Fragmento1())
+                .commit();*/
+
         mBtnCargarFragmento1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //Cargamos el fragmento1
-                getSupportFragmentManager().beginTransaction()
+                CargarFragmento(new Fragmento1());
+                /*getSupportFragmentManager().beginTransaction()
                         .replace(R.id.FL_ContenidoFragmentos, new Fragmento1())
-                        .commit();
+                        .commit();*/
             }
         });
 
@@ -34,10 +41,17 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 //Cargamos el fragmento2
-                getSupportFragmentManager().beginTransaction()
+                CargarFragmento(new Fragmento2());
+                /*getSupportFragmentManager().beginTransaction()
                         .replace(R.id.FL_ContenidoFragmentos, new Fragmento2())
-                        .commit();
+                        .commit();*/
             }
         });
+    }
+
+    private void CargarFragmento(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.FL_ContenidoFragmentos, fragment)
+                .commit();
     }
 }
